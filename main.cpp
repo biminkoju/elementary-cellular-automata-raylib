c2706def30b29a196e3cdb9d95bfc03155642d4e
bimin koju <biminkoju@gmail.com>
2024-01-10 16:44:15 +0545

    shit's fucked

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <bitset>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+#include "raylib.h"
+
+#define GRID_SIZE 10
+
+#define ruleSetNo 88
+struct coords {
+    int x;
+    int y;
+};
+struct celluwu {
+    coords cellCoords;
+    Rectangle cell = {cellCoords.x, cellCoords.y, GRID_SIZE, GRID_SIZE};
+    Color color;
+    int keyVal;
+};
+
+celluwu cellStateMod(const celluwu cell, const celluwu cell1, const celluwu cell2, std::vector<std::pair<int, int>> &ruleset) {
+    celluwu newCell;
+    int checkerKey = cell.keyVal * 4 + cell1.keyVal * 2 + cell2.keyVal;
+    for (size_t i = 0; i < 7; i++) {
+        if (checkerKey == ruleset[i].second) {
+            newCell.keyVal = ruleset[i].first;
+        }
+    }
+    newCell.cellCoords.x = cell.cellCoords.x;
+
+    newCell.cellCoords.y = cell.cellCoords.y + GRID_SIZE;
+
+    newCell.color = (newCell.keyVal == 1 ? BLUE : RED);
+    return newCell;
+}
+
+int main(void) {
+    // Initialization
+    srand(static_cast<unsigned int>(time(0)));
+    std::vector<std::pair<int, int>> ruleSet;
+    if (ruleSetNo >= 0 && ruleSetNo <= 255) {
+        std::bitset<8> binaryRepresentation(ruleSetNo);
+        for (int i = 7; i >= 0; --i) {
+            ruleSet.push_back({binaryRepresentation[i], i});
+        };
+    };
+
+    std::vector<celluwu> initialCellArray;
+    std::vector<celluwu> newinitialCellArray;
+    std::vector<std::vector<celluwu>> screenCells;
+    InitWindow(1000, 600, "window");
+    for (int x = 0; x < GetScreenWidth(); x += GRID_SIZE) {
+        celluwu rCell;
+        rCell.cellCoords = {x, 0};
+        rCell.keyVal = rand() % 2;
+        rCell.color = (rCell.keyVal == 1 ? BLUE : RED);
+        initialCellArray.push_back(rCell);
+    }
+    while (!WindowShouldClose()) {
+        screenCells.push_back(initialCellArray);
+        if (screenCells.size() > GetScreenHeight() / GRID_SIZE) {
+            screenCells.erase(screenCells.begin());
+            std::rotate(screenCells.begin(), screenCells.begin() + 1, screenCells.end());
+
+            // Adjust the y-coordinate of the shifted row
+            for (auto &row : screenCells) {
+                for (auto &cell : row) {
+                    cell.cellCoords.y -= GRID_SIZE;
+                }
+            }
+        };
+        BeginDrawing();
+        for (const auto &rowarray : screenCells) {
+            for (const auto &i : rowarray) {
+                DrawRectangleRec(i.cell, i.color);
+            }
+        }
+
+        EndDrawing();
+        newinitialCellArray.clear();
+        for (size_t i = 0; i < initialCellArray.size(); i++) {
+            size_t a = i, b = (i + 1) % initialCellArray.size(), c = (i + 2) % initialCellArray.size();
+
+            newinitialCellArray.push_back(cellStateMod(initialCellArray[a], initialCellArray[b], initialCellArray[c], ruleSet));
+        }
+        initialCellArray.clear();
+        initialCellArray = std::move(newinitialCellArray);
+    }
+
+    CloseWindow();
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+default:
+	g++ main.cpp -o build -O2 -Wall -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+
